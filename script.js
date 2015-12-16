1832854f53cfe977886eddeeb6ac642c04cec984
patrickb <patrick.belaire@gmail.com>
2015-12-16 09:54:49 +0100

    2

--- script.js
+++ script.js
@@ -12,3 +12,10 @@ $(function()
         }
      });
   });
+  $(function(){
+       setInterval(function(){
+          $(".slideshow ul").animate({marginLeft:-350},800,function(){
+             $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
+          })
+       }, 3500);
+    });
